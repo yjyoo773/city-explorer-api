0888ef0fea6667ce0ce92a7c42ffd02820af7ffa
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-03-23 14:07:06 -0700

    first commit

--- server.js
+++ server.js
@@ -3,18 +3,17 @@
 // calling express library
 const express = require("express");
 // for env
-require('dotenv').config
-const cors = require('cors');
-
-
+require("dotenv").config();
+const cors = require("cors");
 
 // initializing express library
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT;
-app.use(cors())
+// const PORT = 3001;
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-app.listen(PORT,()=>console.log(`listening on ${PORT}`));
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
