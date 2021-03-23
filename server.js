5de0eb5af8787cb55d9da567623ca98f854ac913
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-03-23 15:38:52 -0700

    finish backend of feature2

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const express = require("express");
 // for env
 require("dotenv").config();
 const cors = require("cors");
+// get weather data
+const weather = require("./data/weather.json");
+const weather_data = weather.data;
 
 // initializing express library
 const app = express();
@@ -15,5 +18,23 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+function Forecast(date, description) {
+  this.date = date;
+  this.description = description;
+}
 
+// weather_data.map((x) => Forecast(x.datetime, x.weather.description));
+
+app.get("/weather", (req, res) => {
+  console.log(req.query)
+  res.send({
+    longitude: req.query.lon,
+    latitude: req.query.lat,
+    // forecast: Forecast(weather_data.datetime, weather_data.weather),
+    forecast: weather_data.map((x) =>
+      new Forecast(x.datetime, x.weather.description)
+    ),
+  });
+});
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
