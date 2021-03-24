133714fb7f801cbea72897be6c9a06abba14bb8d
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-03-24 14:45:47 -0700

    finish feature 1

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require("express");
 // for env
 require("dotenv").config();
 const cors = require("cors");
+// SuperAgent
+const superagent = require("superagent");
 // get weather data
 const weather = require("./data/weather.json");
 const weather_data = weather.data;
@@ -23,20 +25,53 @@ function Forecast(date, description) {
   this.description = description;
 }
 
-app.get("/weather", (req, res) => {
+function getLoc(req, res) {
+  console.log(req.query);
+  let lon = req.query.lon;
+  let lat = req.query.lat;
+  let forecastArr = weather_data.map(
+    (x) => new Forecast(x.datetime, x.weather.description)
+  );
   try {
-    console.log(req.query);
-    res.send({
-      longitude: req.query.lon,
-      latitude: req.query.lat,
-      forecast: weather_data.map(
-        (x) => new Forecast(x.datetime, x.weather.description)
-      ),
+    res.status(200).send({
+      longitude: lon,
+      latitude: lat,
+      forecast: forecastArr,
     });
   } catch (err) {
     console.log(err);
     res.status(500).send(err);
   }
-});
+}
+
+  
+  function getForecast(req, res) {
+    let lon = req.query.lon;
+    let lat = req.query.lat;
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily`;
+    let queryWeather = {
+      key: process.env.WEATHER_API_KEY,
+      lat: lat,
+      lon: lon,
+      days: 10,
+    };
+    superagent
+      .get(url)
+      .query(queryWeather)
+      .then((saResults) => {
+        let saData = saResults.body.data
+        let forecastArr = saData.map(
+          (x) => new Forecast(x.datetime, x.weather.description)
+        );
+        res.status(200).send({
+          longitude:lon,
+          latitude:lat,
+          forecast:forecastArr
+        })
+      });
+  }
+
+  // app.get("/weather",getLoc)
+app.get("/weather", getForecast);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
